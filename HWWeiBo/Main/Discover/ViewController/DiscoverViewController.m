446a88c17ab99d84330a5bfd67055283c95d34d3
yang110 <863354274@qq.com>
2015-10-03 15:12:15 +0800

    iOS9_weibo

---
+++ HWWeiBo/Main/Discover/ViewController/DiscoverViewController.m
@@ -0,0 +1,45 @@
+//
+//  DiscoverViewController.m
+//  HWWeiBo
+//
+//  Created by Mac on 15/8/19.
+//  Copyright (c) 2015年 杨梦佳. All rights reserved.
+//
+
+#import "DiscoverViewController.h"
+
+#import "DiscoverNearByViewController.h"
+@interface DiscoverViewController ()
+
+@end
+
+@implementation DiscoverViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    
+    self.navigationController.navigationBar.translucent=YES;
+    
+ 
+}
+
+
+- (IBAction)nearByWeibo:(id)sender
+{
+    
+    
+    
+    DiscoverNearByViewController *vc=[[DiscoverNearByViewController alloc]init];
+    
+
+    [self.navigationController pushViewController:vc animated:YES];
+    
+    
+    
+}
+
+
+
+@end
